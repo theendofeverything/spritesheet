494d2b6f84e7a8d6d5cabd6039230782ad2a8ea9
sustainablelab <sustainablelab@gmail.com>
2022-07-16 11:31:22 -0400

    Rename sprite vars to sprite_frame and sprite_render

--- q.c
+++ q.c
@@ -5,25 +5,26 @@
  * - Select frame to render WHEN COPYING THE TEXTURE TO THE RENDERER:
  *
  *   Example:
- *   SDL_RenderCopy(ren, img_tex, &frame, &sprite);
+ *   SDL_RenderCopy(ren, img_tex, &sprite_frame, &sprite_render);
  *
  *   Rects:
- *   frame : SDL_Rect identify one frame on the sprite sheet
- *   sprite : SDL_Rect defining size and location of rendered frame on screen
+ *   sprite_frame : SDL_Rect identify one frame on the sprite sheet
+ *   sprite_render : SDL_Rect defining size and location of rendered
+ *   frame on screen
  * *******************************/
 /* *************Sprite Sheet: Select Frames***************
  * - Every frame has size sprite_size x sprite_size
  * - Example: rect selects the first frame (x=0, y=0)
  *
- *      SDL_Rect frame = {.x=0*sprite_size, .y=0, .w=sprite_size, .h=sprite_size};
+ *      SDL_Rect sprite_frame = {.x=0, .y=0, .w=sprite_size, .h=sprite_size};
  *
  * - Define the size and location of the sprite frame on the screen.
  * - Example: rect centers the sprite on the screen and renders it to scale
  *
- *      SDL_Rect sprite = { .x=wI.w-sprite_size}/2,
- *                          .y=wI.h-sprite_size}/2,
- *                          .w=sprite_size,
- *                          .h=sprite_size }
+ *      SDL_Rect sprite_render = { .x=wI.w-sprite_size}/2,
+ *                                 .y=wI.h-sprite_size}/2,
+ *                                 .w=sprite_size,
+ *                                 .h=sprite_size }
  *
  * *******************************/
 /* *************TODO***************
@@ -68,6 +69,7 @@ void next_frame(SDL_Rect *frame, bool *run, int sprite_size, int nframes)
         *run = false;
     }
 }
+
 int main(int argc, char *argv[])
 {
     for(int i=0; i<argc; i++) puts(argv[i]);
@@ -99,15 +101,15 @@ int main(int argc, char *argv[])
 
     // Turn spritesheet into sprite animation
     IMG_Init(IMG_INIT_PNG);
-    SDL_Texture *img_tex;
+    SDL_Texture *img_tex;                                       // One texture for entire sprite sheet
     const int sprite_size=64;                                   // Sprite frames are 64x64
     int sprite_scale = 4;                                       // Initial scale is 4x actual size
-    SDL_Rect sprite = { .x=(wI.w-sprite_scale*sprite_size)/2,
-                        .y=(wI.h-sprite_scale*sprite_size)/2,
-                        .w=sprite_scale*sprite_size,
-                        .h=sprite_scale*sprite_size             // Scale sprite up by 4x
-                        };
-    SDL_Rect frame = {  .x=0, .y=0,                             // start at first frame
+    SDL_Rect sprite_render = {.x=(wI.w-sprite_scale*sprite_size)/2, // Initial x-pos: center
+                              .y=(wI.h-sprite_scale*sprite_size)/2, // Initial y-pos: center
+                              .w=sprite_scale*sprite_size,      // Scale sprite up by 4x
+                              .h=sprite_scale*sprite_size       // Scale sprite up by 4x
+                              };
+    SDL_Rect sprite_frame = {  .x=0, .y=0,                             // start at first frame
                         .w=sprite_size, .h=sprite_size          // 64x64 sprite
                         };
     const char *img_path = "art/Kerbey-blinkey-tiredey.png";
@@ -120,7 +122,6 @@ int main(int argc, char *argv[])
             TTF_CloseFont(font); TTF_Quit(); SDL_Quit();
             return EXIT_FAILURE;
         }
-        // Texture to show entire Spritesheet
         img_tex = SDL_CreateTextureFromSurface(ren, img_surf);
         SDL_FreeSurface(img_surf);
     }
@@ -160,7 +161,7 @@ int main(int argc, char *argv[])
                             break;
                         case SDLK_SPACE:
                             run_animation = true;
-                            frame.x = -1*sprite_size; frame.y = 0;    // Reset animation
+                            sprite_frame.x = -1*sprite_size; sprite_frame.y = 0;    // Reset animation
                             break;
                         default: break;
                     }
@@ -175,19 +176,19 @@ int main(int argc, char *argv[])
             {
                 sprite_scale++;
                 if(  sprite_scale>32  ) sprite_scale=32;
-                sprite.x=(wI.w-sprite_scale*sprite_size)/2;
-                sprite.y=(wI.h-sprite_scale*sprite_size)/2;
-                sprite.w=sprite_scale*sprite_size;
-                sprite.h=sprite_scale*sprite_size;
+                sprite_render.x=(wI.w-sprite_scale*sprite_size)/2;
+                sprite_render.y=(wI.h-sprite_scale*sprite_size)/2;
+                sprite_render.w=sprite_scale*sprite_size;
+                sprite_render.h=sprite_scale*sprite_size;
             }
             if(  k[SDL_SCANCODE_DOWN]  )
             {
                 sprite_scale--;
                 if(  sprite_scale<1  ) sprite_scale=1;
-                sprite.x=(wI.w-sprite_scale*sprite_size)/2;
-                sprite.y=(wI.h-sprite_scale*sprite_size)/2;
-                sprite.w=sprite_scale*sprite_size;
-                sprite.h=sprite_scale*sprite_size;
+                sprite_render.x=(wI.w-sprite_scale*sprite_size)/2;
+                sprite_render.y=(wI.h-sprite_scale*sprite_size)/2;
+                sprite_render.w=sprite_scale*sprite_size;
+                sprite_render.h=sprite_scale*sprite_size;
             }
         }
 
@@ -196,7 +197,7 @@ int main(int argc, char *argv[])
             if(  ticks < ticks_per_anim_frame  ) ticks++;
             else
             {
-                next_frame(&frame, &run_animation, sprite_size, 30); // 30 frames
+                next_frame(&sprite_frame, &run_animation, sprite_size, 30); // 30 frames
                 ticks = 0;
             }
         }
@@ -211,8 +212,8 @@ int main(int argc, char *argv[])
             /* SDL_RenderCopy(ren, img_tex, NULL, NULL); */
 
             /* SDL_Rect frame1 = {.x=1*sprite_size, .y=0*sprite_size, .w=sprite_size, .h=sprite_size}; */
-            SDL_RenderCopy(ren, img_tex, &frame, &sprite);
-            /* SDL_RenderCopy(ren, img_tex, &frame1, &sprite); */
+            SDL_RenderCopy(ren, img_tex, &sprite_frame, &sprite_render);
+            /* SDL_RenderCopy(ren, img_tex, &frame1, &sprite_render); */
         }
         if(show_debug)
         { // Debug overlay
